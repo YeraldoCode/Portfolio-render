30277f9c09f754b9bce6269754a5c040ebd8a0e8
Yeraldo Avila <yeramtza@gmail.com>
2025-02-14 03:42:05 -0600

    Configure project for Render deployment
    - Update manage.py to handle port configuration in production
    - Adjust Django and dependency versions in requirements.txt
    - Modify settings.py for compatibility
    - Update urls.py to remove type ignore comment

--- manage.py
+++ manage.py
@@ -6,9 +6,14 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # Configuración para producción en Render
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webpersonal.settings')
     try:
-        from django.core.management import execute_from_command_line # type: ignore
+        from django.core.management import execute_from_command_line  # type: ignore
+        from django.conf import settings
+        if not settings.DEBUG:
+            import django.core.management.commands.runserver as runserver
+            runserver.Command.default_port = os.environ.get('PORT', '8000')
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
